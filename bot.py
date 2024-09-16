04c2f44e5dbfbdc24c3097c9bbf5831346bee5dc
Luck Mix <153753933+lk3140318@users.noreply.github.com>
2024-09-16 11:36:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,9 +3,9 @@ import os
 
 TOKEN = os.environ.get("TOKEN", "")
 
-APP_ID = int(os.environ.get("APP_ID", ""))
+APP_ID = int(os.environ.get("APP_ID", "24335028"))
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "b204ec833fb451fb913fc8e683b232d0")
 
 if __name__ == "__main__" :
     plugins = dict(
